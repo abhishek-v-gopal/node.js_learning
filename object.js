08c61f2a37828bba6323838dbda183e4e0767645
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2023-06-24 09:48:14 +0530

    node.js learning

--- object.js
+++ object.js
@@ -1,7 +1,15 @@
-var person={ name: 'abhishek', age: 19, place: 'oorukary' }
+var person={ name: 'abhishek', age: 19, place: 'oorukary', display:function(){
+    var name="abhi"
+    console.log(this.age)
+} }
 
 for(x in person){
     console.log(person[x])
 }
+person.dob="13/11/2003"
+console.log(person.name,person.age)     
 
-console.log(person.name,person.age) 
+person.displayage=function(){
+    console.log(this.age)
+}
+console.log(person.displayage())
