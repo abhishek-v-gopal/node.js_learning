475b046a5d74e5ff96a1f6a76d662ccf72e41870
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2023-07-06 23:57:24 +0530

    laerning

--- sampleserver.js
+++ sampleserver.js
@@ -1,11 +1,31 @@
 var http=require('http')
 var fs=require('fs')
 http.createServer(function (req,res){
+
+   if(req.url==='/'){
+
     fs.readFile('html.html',function(err,data){
         res.writeHead(200,{'Content-Type':'text/html'})
         res.write(data)
         res.end()
     })
-    
-}).listen(7000)
 
+}else if(req.url==='/login')
+{
+    fs.readFile('signup.html',(err,data)=>{
+        res.writenHead(200,{'content-Type':'text/html'})
+        res.write(data)
+        res.end()
+    })
+  
+}else{
+    res.writeHead(404,{'Content-Type':'text/html'})
+    res.write('error')
+     res.end()
+}
+
+    
+}).listen(7000,function (){
+    console.log('server started')
+})
+    
