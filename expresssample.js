b227794b2d9af8a19dca34f456597cbe4adeb1db
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2023-11-03 11:01:21 +0530

    added express

--- expresssample.js
+++ expresssample.js
@@ -1,9 +1,30 @@
 const express=require('express')
+const path=require('path')
 const app=express()
 
-app.get('/',(req,res)=>{
-    res.send('hello')
+
+app.use((req,res,next)=>{
+   console.log('start')
+    next()
+})
+app.get('/signup',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'signup.html'))
+    console.log('mid')
+    next()
+})
+
+app.use((req,res)=>{
+    console.log('end')
+})
+
+app.post('/signup',(req,res)=> {
+    res.send('account created')
+})
+
+app.get('/about',(req,res)=> {
+    res.send('hai')
 })
 app.listen(3000,()=>{
+    console.log(__filename)
     console.log('server started')
 })
